97ae155370764af5ee5249b131ee26a9485eea0c
rachitkakkar <rachitkakkar7@gmail.com>
2023-08-30 11:36:31 -0500

    Better Upload Styling

--- VideoTrimmer/public/app.js
+++ VideoTrimmer/public/app.js
@@ -4,34 +4,34 @@ let ffmpeg = null;
 
 // Create a function to trim our video without blocking our webpage
 const trim = async ({ target: { files } }) => {
-  const message = document.getElementById('message');
+  // Load ffmpeg
   if (ffmpeg === null) {
       ffmpeg = new FFmpeg();
-      ffmpeg.on("log", ({ message }) => {
-          console.log(message);
-      })
-      ffmpeg.on("progress", ({ progress }) => {
-          message.innerHTML = `${progress * 100} %`;
-      });
       await ffmpeg.load({
           coreURL: "/assets/core/package/dist/umd/ffmpeg-core.js",
       });
   }
+
+  // Write file into name
   const { name } = files[0];
   await ffmpeg.writeFile(name, await fetchFile(files[0]));
+
+  // Start trimming and write out our message
+  const message = document.getElementById('message');
   message.innerHTML = 'Start trimming';
   await ffmpeg.exec(['-i', name, '-ss', '0', '-to', '1', 'output.mp4']);
   message.innerHTML = 'Complete trimming';
-  const data = await ffmpeg.readFile('output.mp4');
-
+  
+  // Download file
+  const data = await ffmpeg.readFile('output.mp4');  
   var a = document.createElement("a");
   document.body.appendChild(a);
   a.style = "display: none";
-
   a.href = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
   a.download = 'trimed-video.mp4';
   a.click();
 }
 
+// When a file is uploaded, run the trim function
 const elm = document.getElementById('uploader');
 elm.addEventListener('change', trim);
